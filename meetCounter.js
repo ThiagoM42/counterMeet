27b941a55291d806b7afa986e1098ff27fdd61f1
Thiago Matos <tms.42@hotmail.com>
2021-07-03 20:51:46 -0300

    add class

--- meetCounter.js
+++ meetCounter.js
@@ -1,15 +1,17 @@
 class countDown{
-	constructor(classe1, classe2){
+	constructor(el_ul_, el_li_){
 		//zoom
 		// let participantes =  document.querySelector('.participants-ul')
-		// let participantes =  document.querySelector('.GvcuGe')		
-		let participantes =  document.querySelector(`.${classe1}`)		
-		this.observer(participantes)
+		// let participantes =  document.querySelector('.GvcuGe')
+		this.el_ul = el_ul_			
+		this.el_li = el_li_	
+		console.log(this.el_li)		
+		this.participantes =  document.querySelector(`.${this.el_ul}`)	
+		this.observer(this.el_ul, this.el_li)
 	}
-	pegarParticipantes(){
-		let participantes_ =  Array.from(document.querySelectorAll(`.${classe2}`))
-		// let participantes_ =  Array.from(document.querySelectorAll('.ZjFb7c'))
-		// let participantes_ =  Array.from(document.querySelectorAll('.participants-item__display-name'))
+	pegarParticipantes(li){
+		let participantes_ =  Array.from(document.querySelectorAll(`.${li}`))
+		console.log(li)
 
 		//verificar se existe essa propriedade;
 		if(!localStorage.hasOwnProperty("participantes")){
@@ -29,18 +31,16 @@ class countDown{
 		console.log('alterado')	 
 	}
 
-	 observer(participantes){
+	 observer(ul, li){
 		try{
-			let observer = new MutationObserver(this.pegarParticipantes)
-			observer.observe(participantes, {childList:true});
+			let observer = new MutationObserver(()=>this.pegarParticipantes(this.el_li))
+			observer.observe(this.participantes, {childList:true});
 		}catch(error){
-			console.log(error)			
-			setTimeout(function(){new countDown}, 15000)
+			console.log(error)								
+			setTimeout(function(){new countDown(ul, li) }, 6000)
 			
 		}
 	}
 }
 
 new countDown('GvcuGe', 'ZjFb7c'); 
-//new countDown('participants-ul', 'participants-item__display-name'); 
-
